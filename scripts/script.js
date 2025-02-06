7d699d4e88732f0a67ac3514a9f11c29494de601
LouisPino <ljpino95@gmail.com>
2025-02-05 20:34:09 -0500

    fullscreen experiments

--- scripts/script.js
+++ scripts/script.js
@@ -132,9 +132,25 @@ function callFunction(locationName) {
 }
 
 
+// Select the element we want to make fullscreen. 
+// Often 'document.documentElement' or a specific element like a video container.
+// const elem = document.documentElement;
+
+// // Cross-browser function for requesting fullscreen
+// function openFullscreen() {
+//     if (elem.requestFullscreen) {
+//         elem.requestFullscreen();
+//     } else if (elem.mozRequestFullScreen) { /* Firefox */
+//         elem.mozRequestFullScreen();
+//     } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
+//         elem.webkitRequestFullscreen();
+//     } else if (elem.msRequestFullscreen) { /* IE/Edge */
+//         elem.msRequestFullscreen();
+//     }
+// }
 
 
-
+// document.getElementById("fullscreenBtn").addEventListener("click", openFullscreen);
 
 
 

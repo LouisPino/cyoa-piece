050ff55173f013ccd9cab5c503c69fe20faadf5c
LouisPino <ljpino95@gmail.com>
2025-03-19 21:01:47 -0400

    mobile voting done

--- mobile/scripts/helpers/vote.js
+++ mobile/scripts/helpers/vote.js
@@ -1,14 +1,36 @@
 function startVote(section) {
-    console.log(section)
     mainEl.innerHTML = extras.filter((extra) => (extra.name === "vote"))[0].content//get vote html
     let voteAImgEl = document.getElementById('vote-a-img')
     let voteBImgEl = document.getElementById('vote-b-img')
+    let voteABgEl = document.getElementById('vote-a-bg')
+    let voteBBgEl = document.getElementById('vote-b-bg')
     let prompt = document.getElementById('vote-banner-text')
     prompt.innerHTML = section.choicePrompt
     voteAImgEl.src = `/mobile/assets/vote/location/${section.choices[0]}/up.PNG`
     voteBImgEl.src = `/mobile/assets/vote/location/${section.choices[1]}/up.PNG`
     voteAImgEl.addEventListener('click', () => handleVote("choice1"));
     voteBImgEl.addEventListener('click', () => handleVote("choice2"));
+    voteAImgEl.addEventListener("touchstart", () => {
+        voteAImgEl.src = voteAImgEl.src.replace("up", "down");
+        voteABgEl.src = voteABgEl.src.replace("up", "down");
+    });
+    voteAImgEl.addEventListener("mouseup", () => {
+        voteAImgEl.src = voteAImgEl.src.replace("down", "up");
+        voteABgEl.src = voteABgEl.src.replace("down", "up");
+    });
+
+
+    voteBImgEl.addEventListener("touchstart", () => {
+        voteBImgEl.src = voteBImgEl.src.replace("up", "down");
+        voteBBgEl.src = voteBBgEl.src.replace("up", "down");
+    });
+
+    voteAImgEl.addEventListener("mouseup", () => {
+        voteBImgEl.src = voteBImgEl.src.replace("down", "up");
+        voteBBgEl.src = voteBBgEl.src.replace("down", "up");
+    });
+
+
 }
 
 
@@ -19,6 +41,7 @@ function startSkinVote(item) {
     const choice2El = document.getElementById("skin-choice2")
     choice1El.src = item.choices[0].img
     choice2El.src = item.choices[1].img
+
     choice1El.addEventListener('click', () => handleVote("choice1"));
     choice2El.addEventListener('click', () => handleVote("choice2"));
 }
@@ -27,4 +50,17 @@ function startSkinVote(item) {
 function handleVote(vote) {
     sendToServer({ type: "vote", val: vote })
     mainEl.innerHTML = extras.filter((extra) => (extra.name === "thank"))[0].content
+}
+
+function flashOnce(voteEls) {
+    voteEls.forEach((el) => {
+        setTimeout(() => {
+            el.src = el.src.replace("up", "down");
+        }, flashTime / 2);
+
+        setTimeout(() => {
+            el.src = el.src.replace("down", "up"); // Change back if needed
+        }, flashTime);
+    });
+
 }

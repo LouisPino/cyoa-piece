2357ddf66d0c6f2a0e7245a4712347fc2d1db70c
LouisPino <ljpino95@gmail.com>
2025-04-25 21:19:43 -0400

    atarting chat

---
+++ display/scripts/locations/chat.js
@@ -0,0 +1,22 @@
+function chat() {
+    toggleAnimation("side", "jaz")
+    toggleAnimation("side", "pino")
+    slideBoxY("none")
+    jumpChar("npc", 550, 0)
+    changeSize("npc", 1, .7)
+    jumpChar("duo", 0, 50)
+    changeSize("duo", 1, .8)
+    if (history.locationsVisited[history.locationsVisited.length - 2].name === "rats") {
+        // changeBg("animated/riverInterior.gif")
+        document.getElementById("sandbox-bg").src = "/display/assets/backgrounds/animated/riverInterior.gif"
+    } else {
+        // changeBg("animated/caveInterior.gif")
+        document.getElementById("sandbox-bg").src = "/display/assets/backgrounds/animated/caveInterior.gif"
+    }
+    renderPino()
+    renderJaz()
+    changeNPCSrc(`${history.locationsVisited[history.locationsVisited.length - 2].name.slice(0, -1)}/full.png`)
+}
+
+
+chat()
